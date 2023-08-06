70ea7cc6e467f24fe951e8bab21ad13ed6d37ae1
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2023-08-06 22:39:12 +0200

    Create First.sol

---
+++ MiniProject/01_First_Smart_Contract/First.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+// ^ License on which our SMART contract will operate ^ 
+// ^ Licencja na której operować będzie nasz SMART kontrakt ^
+
+
+pragma solidity 0.8.18;
+// ^ The language we work on ^
+// ^ Język na którym pracujemy ^
+
+contract MyContract {
+
+    string public first_string = "Hello World";
+
+}
